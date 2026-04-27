1402ab150b202d16cde4f9bf934dd1f025b1289d
Dhruti Haria <dhrutiharia05@gmail.com>
2026-04-27 21:24:19 +0530

    fixed cors slash issue

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173', 'https://mern-auth-three-beta.vercel.app/']
+const allowedOrigins = ['http://localhost:5173', 'https://mern-auth-three-beta.vercel.app']
 
 app.use(express.json());
 app.use(cookieParser());
